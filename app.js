b022376f943c73b5527b8c3b6acd0e2a38e88934
Christian Ariel Munich <munich.christianariel@gmail.com>
2022-01-08 13:45:21 -0300

    uploading project

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const port = 4000;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+app.get('/home',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
+
+app.listen(port,() => console.log(`Server running in http://localhost:${port}`))
+
